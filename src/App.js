f643be89747b8ba7aa512664630ad44c94f5f9f4
Joseph Nylund <joeynylund@yahoo.com>
2020-04-26 18:28:26 -0400

    Adding Cache

--- src/App.js
+++ src/App.js
@@ -18,61 +18,99 @@ function App() {
 
   var streams = [];
 
-async function loadStreams() {
+  var time = new Date()
 
-  var token = ''
+  var cachedTime = '';
 
-  var auth = await fetch('https://id.twitch.tv/oauth2/token?client_id=jrhhhmgv1e73eq5qnswjqh2p3u1uqr&client_secret=ftkfalr4ztrnj1lpn1cgm61elygbxz&grant_type=client_credentials', {
+  var currentTime = time.getTime();
+
+  async function loadStreams() {
+  
+    var token = ''
+
+    var auth = await fetch('https://id.twitch.tv/oauth2/token?client_id=jrhhhmgv1e73eq5qnswjqh2p3u1uqr&client_secret=ftkfalr4ztrnj1lpn1cgm61elygbxz&grant_type=client_credentials', {
       method: 'POST'
     }).then((response) => response.json())
     .then((data) => {
       token = 'Bearer ' + data.access_token;
     })
-
   
-  var cursor = '';
+    var cursor = '';
   
     do {
-    var load = await fetch('https://api.twitch.tv/helix/streams?game_id=516575&first=100&language=en&after=' + cursor + '', {
-    headers: {
-      'Client-ID': 'jrhhhmgv1e73eq5qnswjqh2p3u1uqr',
-      'Authorization': token,
-    }
-    }).then((response) => response.json())
-    .then((data) => {
-      data.data.forEach(stream => {
-        if(stream.viewer_count <= 50) {
-          let live = { 
-            user_id: stream.user_id,
-            user_name: stream.user_name, 
-            title: stream.title, 
+      var load = await fetch('https://api.twitch.tv/helix/streams?game_id=516575&first=100&language=en&after=' + cursor + '', {
+      headers: {
+        'Client-ID': 'jrhhhmgv1e73eq5qnswjqh2p3u1uqr',
+        'Authorization': token,
+      }
+      }).then((response) => response.json())
+      .then((data) => {
+        data.data.forEach(stream => {
+          if(stream.viewer_count <= 50) {
+            let live = { 
+              user_id: stream.user_id,
+              user_name: stream.user_name, 
+              title: stream.title, 
+            }
+            streams.push(live)
+            setLiveStreams(oldArray => [...oldArray, live])
           }
-          streams.push(live)
-          setLiveStreams(oldArray => [...oldArray, live])
-        }
+        })
+        cursor = data.pagination.cursor
       })
-      cursor = data.pagination.cursor
-    })
-  }while(cursor !== undefined)
+    } while(cursor !== undefined)
 
-  console.log(streams.length)
+    var addedTime = time.setMinutes(time.getMinutes() + 5)
 
-  sessionStorage.setItem('streamsArray', JSON.stringify(streams))
+    let cachedTime = addedTime.getTime()
 
-  randomStream = streams[Math.floor(Math.random()*streams.length)];
+    sessionStorage.removeItem('time')
 
-  setChannel(randomStream.user_name)
+    sessionStorage.setItem('time', cachedTime)
 
-  setTitle(randomStream.title)
+    sessionStorage.setItem('streamsArray', JSON.stringify(streams))
 
-}
-useEffect(() => {
-  loadStreams()
-}, [height]);
+    randomStream = streams[Math.floor(Math.random()*streams.length)];
+
+    setChannel(randomStream.user_name)
+
+    setTitle(randomStream.title)
+
+  }
+
+  async function loadFromCache() {
+  
+    streams = JSON.parse(sessionStorage.getItem('streamsArray'))
+
+    randomStream = streams[Math.floor(Math.random()*streams.length)];
+
+    setChannel(randomStream.user_name)
+
+    setTitle(randomStream.title)
+
+  }
+
+  useEffect(() => {
+
+    cachedTime = sessionStorage.getItem('time')
+
+    if (currentTime > cachedTime) {
+      
+      console.log("Pulling Streams!")
+      
+      loadStreams()
+
+    } else {
+
+      console.log("Cached Streams!")
+      
+      loadFromCache()
+
+    }
+  
+  }, [height]);
 
   return (
-
-    
 
     <div className="App">
 
@@ -86,15 +124,13 @@ useEffect(() => {
 
               { title === '' ? <h2>Shuffling the deck of streams...</h2> : <ReactTwitchEmbedVideo theme="dark" width="100%" height={height} channel={channel} /> }  
 
-              </Container>
+          </Container>
 
-           </header>
+        </header>
 
     </div>
 
   );
 }
-
-
 
 export default App;
